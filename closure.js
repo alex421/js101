d79961fd45ae38d0bfaa83ab605c3d17eaad60e4
alex421 <alex421@gmail.com>
2017-01-04 13:17:12 +0200

    added some comments

--- closure.js
+++ closure.js
@@ -11,10 +11,13 @@ return alerter
 
 }
 
-var funcStorer = closureAlert();
 
-funcStorer();
-funcStorer();
+// the value stored in funcStorer is what closureAlert returns which is alerter
+//when we refer later tu funcStorer()  it will call alerter
+var funcStorer = closureAlert();   // closureAlert is being called only once 
+
+funcStorer();// when we reference it later here , it will be actually the function alerter
+funcStorer();// if it were to call closureAlert again , we would be resetting X = 0
 
 var funcStorer2 = closureAlert ();
 
