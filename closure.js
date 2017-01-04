c35ddf433f821c3e85f5b790481ce9774ee12ff9
alex421 <alex421@gmail.com>
2017-01-04 13:20:20 +0200

    indentation

--- closure.js
+++ closure.js
@@ -1,24 +1,27 @@
 var closureAlert = function() {
-var x =0;
+	var x = 0;
 
-    var alerter = function () {
+	var alerter = function() {
 
-   		 alert(++x);
-    };
-console.log(alerter)
+		alert(++x);
+	};
+	console.log(alerter)
 
-return alerter
+	return alerter
 
 }
 
 
 // the value stored in funcStorer is what closureAlert returns which is alerter
 //when we refer later tu funcStorer()  it will call alerter
-var funcStorer = closureAlert();   // closureAlert is being called only once 
+var funcStorer = closureAlert(); // closureAlert is being called only once 
 
-funcStorer();// when we reference it later here , it will be actually the function alerter
-funcStorer();// if it were to call closureAlert again , we would be resetting X = 0
+funcStorer(); // when we reference it later here , it will be actually the function alerter
+funcStorer(); // if it were to call closureAlert again , we would be resetting X = 0
 
-var funcStorer2 = closureAlert ();
+var funcStorer2 = closureAlert();
 
 funcStorer();
+
+
+//
